4175e586be57146dabb61c0d64e47063f9356bd4
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-05 15:49:11 +0900

    feat: in progress solution and submitted solution api route

--- server/src/main/java/com/sb/api/SolutionApi.java
+++ server/src/main/java/com/sb/api/SolutionApi.java
@@ -9,6 +9,7 @@ import com.sb.application.solution.StartSolutionRequest;
 import com.sb.application.solution.SubmitSolutionRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,17 @@ public class SolutionApi {
     public SolutionApi(SolutionService solutionService) {
         this.solutionService = solutionService;
     }
+
+    @GetMapping("/solutions/me/in-progress")
+    public ResponseEntity<Void> getInProgressSolution(@Auth Accessor accessor) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/solutions/me/submitted")
+    public ResponseEntity<Void> getSubmittedSolution(@Auth Accessor accessor) {
+        return ResponseEntity.ok().build();
+    }
+
 
     @PostMapping("/solutions/start")
     public ResponseEntity<SolutionResponse> startSolution(
