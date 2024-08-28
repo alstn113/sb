2e06280ff8887c26ddb1d95921652f8b15d1d08e
김민수 MINSOO KIM <alstn113@gmail.com>
2024-08-28 11:11:29 +0900

    refactor: 인증 확장성 고려

--- server/src/main/java/com/sb/api/common/CookieUtils.java
+++ server/src/main/java/com/sb/api/common/CookieUtils.java
@@ -1,6 +1,5 @@
 package com.sb.api.common;
 
-import java.util.List;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 
@@ -8,36 +7,32 @@ public class CookieUtils {
 
     private static final String TOKEN_COOKIE_NAME = "token";
     private static final int COOKIE_MAX_AGE_ONE_DAY = 60 * 60 * 24;
-    private static final List<String> DOMAINS = List.of(".alstn113.pro", "localhost");
+    private static final String DOMAIN = ".alstn113.pro";
 
     private CookieUtils() {
     }
 
     public static void setTokenCookie(HttpServletResponse response, String token) {
-        DOMAINS.forEach(domain -> {
-            ResponseCookie cookie = ResponseCookie.from(TOKEN_COOKIE_NAME, token)
-                    .maxAge(COOKIE_MAX_AGE_ONE_DAY)
-                    .httpOnly(true)
-                    .secure(true)
-                    .domain(domain)
-                    .path("/")
-                    .build();
+        ResponseCookie cookie = ResponseCookie.from(TOKEN_COOKIE_NAME, token)
+                .maxAge(COOKIE_MAX_AGE_ONE_DAY)
+                .httpOnly(true)
+                .secure(true)
+                .domain(DOMAIN)
+                .path("/")
+                .build();
 
-            response.setHeader("Set-Cookie", cookie.toString());
-        });
+        response.setHeader("Set-Cookie", cookie.toString());
     }
 
     public static void clearTokenCookie(HttpServletResponse response) {
-        DOMAINS.forEach(domain -> {
-            ResponseCookie cookie = ResponseCookie.from(TOKEN_COOKIE_NAME, "")
-                    .maxAge(0)
-                    .httpOnly(true)
-                    .secure(true)
-                    .domain(domain)
-                    .path("/")
-                    .build();
+        ResponseCookie cookie = ResponseCookie.from(TOKEN_COOKIE_NAME, "")
+                .maxAge(0)
+                .httpOnly(true)
+                .secure(true)
+                .domain(DOMAIN)
+                .path("/")
+                .build();
 
-            response.setHeader("Set-Cookie", cookie.toString());
-        });
+        response.setHeader("Set-Cookie", cookie.toString());
     }
 }
