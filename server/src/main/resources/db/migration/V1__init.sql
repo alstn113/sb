4b09256fb6c1142a9cf68db2e3985e555d6945e2
김민수 MINSOO KIM <alstn113@gmail.com>
2024-09-02 16:33:10 +0900

    feat: flyway 수정

--- server/src/main/resources/db/migration/V1__init.sql
+++ server/src/main/resources/db/migration/V1__init.sql
@@ -10,59 +10,6 @@ CREATE TABLE member
     provider     ENUM ('GITHUB') NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE solution
-(
-    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    created_at   TIMESTAMP(6) NOT NULL,
-    member_id    BIGINT       NOT NULL,
-    mission_id   BIGINT       NOT NULL,
-    submitted_at TIMESTAMP(6),
-    description  TEXT,
-    title        VARCHAR(255),
-    url          VARCHAR(255),
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (mission_id) REFERENCES mission (id)
-) ENGINE = InnoDB;
-
-
-CREATE TABLE mission
-(
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    description TEXT                        NOT NULL,
-    thumbnail   TEXT                        NOT NULL,
-    title       VARCHAR(255)                NOT NULL,
-    url         VARCHAR(255)                NOT NULL,
-    language    ENUM ('JAVA', 'JAVASCRIPT') NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE tag
-(
-    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE mission_tag
-(
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    mission_id BIGINT NOT NULL,
-    tag_id     BIGINT NOT NULL,
-    FOREIGN KEY (mission_id) REFERENCES mission (id),
-    FOREIGN KEY (tag_id) REFERENCES tag (id)
-) ENGINE = InnoDB;
-
-CREATE TABLE solution_comment
-(
-    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
-    created_at        TIMESTAMP(6) NOT NULL,
-    deleted_at        TIMESTAMP(6),
-    member_id         BIGINT       NOT NULL,
-    parent_comment_id BIGINT,
-    solution_id       BIGINT       NOT NULL,
-    content           TEXT         NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (solution_id) REFERENCES solution (id)
-) ENGINE = InnoDB;
-
 CREATE TABLE form
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -104,6 +51,31 @@ CREATE TABLE choice
     FOREIGN KEY (question_id) REFERENCES question (id)
 ) ENGINE = InnoDB;
 
+CREATE TABLE mission
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    description TEXT                        NOT NULL,
+    thumbnail   TEXT                        NOT NULL,
+    title       VARCHAR(255)                NOT NULL,
+    url         VARCHAR(255)                NOT NULL,
+    language    ENUM ('JAVA', 'JAVASCRIPT') NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE tag
+(
+    id   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE mission_tag
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    mission_id BIGINT NOT NULL,
+    tag_id     BIGINT NOT NULL,
+    FOREIGN KEY (mission_id) REFERENCES mission (id),
+    FOREIGN KEY (tag_id) REFERENCES tag (id)
+) ENGINE = InnoDB;
+
 CREATE TABLE option
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -113,3 +85,29 @@ CREATE TABLE option
     FOREIGN KEY (question_id) REFERENCES question (id)
 ) ENGINE = InnoDB;
 
+CREATE TABLE solution
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    created_at   TIMESTAMP(6) NOT NULL,
+    member_id    BIGINT       NOT NULL,
+    mission_id   BIGINT       NOT NULL,
+    submitted_at TIMESTAMP(6),
+    description  TEXT,
+    title        VARCHAR(255),
+    url          VARCHAR(255),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (mission_id) REFERENCES mission (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE solution_comment
+(
+    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
+    created_at        TIMESTAMP(6) NOT NULL,
+    deleted_at        TIMESTAMP(6),
+    member_id         BIGINT       NOT NULL,
+    parent_comment_id BIGINT,
+    solution_id       BIGINT       NOT NULL,
+    content           TEXT         NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (solution_id) REFERENCES solution (id)
+) ENGINE = InnoDB;
